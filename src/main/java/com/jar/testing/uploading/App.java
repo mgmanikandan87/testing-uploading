63128b6213e36043676c441f0b51ea2b700005f6
mgmanikandan87 <mgmanikandan87@gmail.com>
2015-08-05 23:26:27 +0530

    first commit

---
+++ src/main/java/com/jar/testing/uploading/App.java
@@ -0,0 +1,16 @@
+package com.jar.testing.uploading;
+
+
+/**
+ * @author manikandan
+ *
+ */
+public class App {
+  public int getSumOfTwoNumber(int a, int b) {
+    return a + b;
+  }
+
+  public int getSubOfTwoNumber(int a, int b) {
+    return a - b;
+  }
+}
